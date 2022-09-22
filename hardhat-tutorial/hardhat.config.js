d0880216bb9a81f906ee1efbbae0e99d2e245022
lkbstan <95829097+lkbstan@users.noreply.github.com>
2022-09-22 13:40:39 +0100

    first commit

---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const { QUICKNODE_HTTP_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
